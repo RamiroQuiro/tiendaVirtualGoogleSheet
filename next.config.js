b0936f25d72fd6868dbb756416ee3adad4ac2bd2
ramiroquiro <rama.exe.13@gmail.com>
2023-02-26 12:38:16 -0300

    configuraicones imagen

--- next.config.js
+++ next.config.js
@@ -1,8 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
   images: {
     domains:["www.dropbox.com","lh3.googleusercontent.com","drive.google.com"],
-    },
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'www.dropbox.com',
+        port: '',
+        pathname: '/s/**',
+      },
+    ],
+  },
   experimental: {
     appDir: true,
   },
